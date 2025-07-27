290f708069a9a9949491895970eb9db89b3fa6fe
Andrian Baros <andrianbaros10@gmail.com>
2025-07-27 15:09:58 +0700

    last

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.playmood"
+        applicationId = "com.barudakdev.playmood"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
